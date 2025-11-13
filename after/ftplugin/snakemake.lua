889e5328a7db90b3c963bee6878e55939a4300f4
ZarulHanifah <zarulsaurus@gmail.com>
2025-11-13 12:57:00 +0800

    New stuff

---
+++ after/ftplugin/snakemake.lua
@@ -0,0 +1,4 @@
+vim.bo.expandtab = true -- Convert tab to spaces
+vim.bo.shiftwidth = 4 -- Indent size
+vim.bo.tabstop = 4 -- Tab width when displaying
+vim.bo.softtabstop = 4 -- Number of spaces a <Tab> counts for in insert mode
